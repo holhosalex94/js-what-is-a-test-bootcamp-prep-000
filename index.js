17e2b254ba8357fcaa94e9e0357c801a85ccd936
Alex Holhos <alex.holhos@emanuel.ro>
2018-02-03 13:25:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var message = `${name} is ${height} inches tall`;
 module.exports = {name, height, message}
   describe("Message", () => {
     it("gives the name and height", () => {
-      expect(index.message).toInclude(index.name)
-      expect(index.message).toInclude(index.height)
+      expect(message).toInclude(name)
+      expect(message).toInclude(height)
     })
   })
